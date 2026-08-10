eeee334a2c55747f1360a3a597eefdde66be0118
Grant Achuzia <grant.achuzia@gmail.com>
2026-08-10 13:24:04 -0400

    feat: add plugins.lua and move plugins out of init.lua

---
+++ lua/plugins.lua
@@ -0,0 +1,52 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup()
+      vim.cmd.colorscheme("catppuccin")
+    end,
+  },
+  {
+    "nvim-tree/nvim-web-devicons",
+    lazy = false,
+    opts = { default = true },
+  },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "nvim-tree/nvim-web-devicons", -- optional, but recommended
+    },
+    lazy = false, -- neo-tree will lazily load itself
+  },
+
+  {
+    "nvim-telescope/telescope.nvim",
+    version = "*",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    },
+    config = function()
+      local builtin = require("telescope.builtin")
+      vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    branch = "master",
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = { "lua", "javascript", "python", "go", "ruby", "c" },
+        highlight = { enable = true },
+        indent = { enable = true },
+      })
+    end,
+  },
+}
