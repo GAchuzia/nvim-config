fcd4fe53edf4d5d150baf659182cae762b374ad7
Grant Achuzia <grant.achuzia@gmail.com>
2026-08-10 12:26:52 -0400

    feat: add neo tree plugin

--- init.lua
+++ init.lua
@@ -43,6 +43,17 @@ require("lazy").setup({
     end,
   },
   {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "nvim-tree/nvim-web-devicons", -- optional, but recommended
+    },
+    lazy = false, -- neo-tree will lazily load itself
+  },
+
+  {
     "nvim-telescope/telescope.nvim",
     version = "*",
     dependencies = {
