90c031291bbc28828aeb005e1df337eb145e86f9
Grant Achuzia <grant.achuzia@gmail.com>
2026-08-11 03:08:14 -0400

    feat: update vim options for tabs, spaces, autoindent, etc.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -49,4 +49,14 @@ return {
       })
     end,
   },
+  {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    config = function()
+      require("nvim-autopairs").setup({
+        check_ts = true,
+        disable_filetype = { "TelescopePrompt", "vim" },
+      })
+    end,
+  },
 }
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,5 +1,15 @@
 
 vim.g.mapleader = " "
+
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.smartindent = true
+vim.opt.autoindent = true
+vim.opt.smarttab = true
+vim.opt.backspace = { 'indent', 'eol', 'start' }
+
 vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
 
 
